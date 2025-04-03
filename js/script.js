e60f6cef2428f061d64c2fe46b28351673a01a13
ErikaArci99 <erikaarci1999@outlook.it>
2025-04-03 15:58:31 +0200

    [DONE] esercizio terminato con già entrambi i bonus

--- js/script.js
+++ js/script.js
@@ -6,14 +6,17 @@ const listEmail = document.getElementById('listEmail');
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 // ciclo for che mi crea 10 email 
-for (let i = 0; i < 10; i++) {
-    axios.get(endpoint).then( res => {
-        const emails = res.data.response;
-        // console.log(emails);
-        const emailItem = document.createElement('p'); 
-        emailItem.textContent = emails; 
-        listEmail.appendChild(emailItem)
-    })
+function generateEmails() {
+    listEmail.innerHTML = '';  
+    for (let i = 0; i < 10; i++) {
+        axios.get(endpoint).then(res => {
+            const emails = res.data.response;
+            const emailItem = document.createElement('p'); 
+            emailItem.textContent = emails; 
+            listEmail.appendChild(emailItem);
+        });
+    }
 }
 
-
+// bottone 
+button.addEventListener('click', generateEmails);
